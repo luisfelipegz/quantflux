6856907508c11e2709099890e11f973d5d689a6e
Luis Felipe Gutierrez Zagazeta <luis.felipe.gutierrez.zagazeta@cern.ch>
2026-03-09 20:24:36 -0400

    Added CLT simulated means plotting

--- probstat/probmethods.py
+++ probstat/probmethods.py
@@ -3,26 +3,36 @@ from scipy.stats import skew
 from typing import Tuple
 import matplotlib.pyplot as plt
 
-def centralLimitTheorem(simEvents: np.ndarray, meanExp: float)->None:
+def centralLimitTheorem(simEvents: np.ndarray, meanExp: float, plot: bool)->None:
   '''
   The average of multiple large random experiments approximates a normal distribution.
   Expect the 68-95-99.7 rule to apply
   Arguments:
-    simEvents: NxM NumPy array (N=number of experiments, M=events per experiment).
-               Each entry corresponds to the output of each experiment
-    meanExp: Expected mean of distribution
+    simEvents : NxM NumPy array (N=number of experiments, M=events per experiment).
+                Each entry corresponds to the output of each experiment
+    meanExp   : Expected mean of distribution
+    plot      : Plotting option
   Returns:
     None
     Just prints the simulated number of events within 1-3 standard deviations
   '''
+  # Obtain means and std
   simulatedMeans = np.mean(simEvents, axis=1)
   simulatedStd = np.std(simulatedMeans)
-  print(simulatedMeans)
-  print(simulatedStd)
+  # Plot means distribution
+  if plot:
+    plt.figure(figsize=(8,6))
+    plt.title("Simulated Means")
+    plt.xlabel("Mean")
+    plt.ylabel("Occurrences")
+    plt.hist(simulatedMeans,bins=25)
+    plt.axvline(meanExp, linestyle="-", color="grey", label=f"Expected Mean={meanExp:.2f} (sim. std={simulatedStd:.2f})")
+    plt.legend(frameon=False)
+    plt.savefig("cltMeans.pdf")
+  # Get stats
   withinOneStd = np.abs(simulatedMeans-meanExp) <= 1*simulatedStd
   withinTwoStd = np.abs(simulatedMeans-meanExp) <= 2*simulatedStd
   withinThreeStd = np.abs(simulatedMeans-meanExp) <= 3*simulatedStd
-  print(withinOneStd)
   print(f'Percentage of simulated means within one std of true mean: {np.mean(withinOneStd)*100:.2f}%')
   print(f'Percentage of simulated means within two std of true mean: {np.mean(withinTwoStd)*100:.2f}%')
   print(f'Percentage of simulated means within three std of true mean: {np.mean(withinThreeStd)*100:.2f}%')
@@ -67,9 +77,3 @@ def fairDiceRollEV(size: int)->int:
   # Return probabilistic EV
   print(f"Average of {size} dice rolls is {avg}")
   return avg
-
-if __name__=="__main__":
-  nRolls = 500
-  nExp = 1000
-  rolls = twoDiceRollSum(nRolls,nExp)
-  centralLimitTheorem(rolls,7)
